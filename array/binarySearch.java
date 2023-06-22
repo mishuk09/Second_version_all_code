5dece267bb28fc3f50d48381feb198d206e5c85f
mishuk09 <mishukinfo09@gmail.com>
2023-06-22 10:35:57 +0530

    BinarySearach

---
+++ array/binarySearch.java
@@ -0,0 +1,35 @@
+package array;
+
+// import javax.xml.stream.events.EndElement;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        int target = 11;
+        int result = BinarySearch(arr, target);
+        System.out.println(result);
+
+    }
+
+    static int BinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+
+    }
+
+}
