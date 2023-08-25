c3e776384d255570e03c78105d25531b091a53c6
mishuk09 <mishukinfo09@gmail.com>
2023-08-25 20:36:04 +0530

    DSA

---
+++ array/twoDarray/basicCOde.java
@@ -0,0 +1,10 @@
+public class basicCOde {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = 0; i < num; i++) {
+            System.out.println(i);
+
+        }
+    }
+
+}
---
+++ array/twoDarray/indexone.java
@@ -0,0 +1,21 @@
+
+public class indexone {
+
+    static void function(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
+        function(arr);
+
+    }
+
+}
