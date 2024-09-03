b7759fc0cc12139cb62608583fbd33e56a072f19
mishuk09 <mishukinfo09@gmail.com>
2024-09-03 13:36:06 +0530

    update basic java

--- Master/df.java
+++ Master/df.java
@@ -1,13 +1,11 @@
 package Master;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class df {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number:");
-        double num1 = sc.nextDouble();
-
-        System.out.println("Sum:" + (num1 * num1));
+    public static void main(String m[]) {
+        int a = Integer.parseInt(m[0]);
+        int b = Integer.parseInt(m[1]);
+        System.out.println(a + b);
     }
 }
